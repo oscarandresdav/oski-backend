492a7d2d2066232f22d77e5c6a1f0c04a4336b66
Oscar Davila <oscar7822002@gmail.com>
2024-03-19 17:27:55 -0500

    Handling Update and Delete Requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    {
+      id: '1',
+      username: 'oscarandresdav',
+      email: 'oscar@mail.com',
+      password: 'password',
+      status: true,
+    },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((item) => item.id === id);
+  }
+
+  create(createUserDto: any) {
+    this.users.push(createUserDto);
+  }
+
+  update(id: string, updateUserDto: any) {
+    const existingUser = this.findOne(id);
+    if (existingUser) {
+      // update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const userIndex = this.users.findIndex((item) => item.id === id);
+    if (userIndex >= 0) {
+      this.users.splice(userIndex, 1);
+    }
+  }
+}
