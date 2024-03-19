b76aa680b7f8ef4d4a17d1634295d44fa93bbc43
Oscar Davila <oscar7822002@gmail.com>
2024-03-19 17:09:53 -0500

    Use Route Parameters

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get()
   findAll() {
     return 'This action returns all users';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} user`;
   }
 }
