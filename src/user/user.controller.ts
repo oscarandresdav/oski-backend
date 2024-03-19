82e76d2a4035549b289917050ff1cc176aa3a4b2
Oscar Davila <oscar7822002@gmail.com>
2024-03-19 17:16:57 -0500

    Handling Request Body / Payload

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +10,11 @@ export class UserController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #${id} user`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+    // return `This action creates a user`
   }
 }
