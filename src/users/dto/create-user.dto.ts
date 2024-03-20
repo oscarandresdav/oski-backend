27e3efa6e52b6b4ce786866b17202b524a7e4a22
Oscar Davila <oscar7822002@gmail.com>
2024-03-20 17:58:39 -0500

    Validate Input Data with Data Transfer Objects
    Tomar en cuenta el cambio en el main.ts
    
    Comandos usados
    - npm i @nestjs/mapped-types
    - npm i class-validator class-transformer

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import { IsBoolean, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   readonly username: string;
+
+  @IsString()
   readonly email: string;
+
+  @IsString()
   readonly password: string;
+
+  @IsBoolean()
   readonly status: boolean;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,4 @@
-export class UpdateUserDto {
-  readonly username?: string;
-  readonly email?: string;
-  readonly password?: string;
-  readonly status?: boolean;
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
