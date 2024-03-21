f9d5d030426ce2f54af0ceaa03a9ed350994705a
Oscar Davila <oscar7822002@gmail.com>
2024-03-21 12:50:12 -0500

    Creating a TypeORM Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ length: 16, unique: true })
   username: string;
+
+  @Column({ length: 255 })
   email: string;
+
+  @Column({ length: 32 })
   password: string;
+
+  @Column({ default: true })
   status: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  modified_at: Date;
+
+  @VersionColumn({ nullable: true })
+  revision: number;
 }
